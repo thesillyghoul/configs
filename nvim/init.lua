894c5076770e392ed18d426bf18bf25498d1e5e7
Dipkiran Shrestha <thesillyghoul@gmail.com>
2026-03-14 15:26:30 +0545

    feat: add lazy.nvim bootstrap

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,20 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
+vim.g.have_nerd_font = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup("plugins")
 require("core.options")
 require("core.keymaps")

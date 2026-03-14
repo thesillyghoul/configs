65d7a69388f9a810e6a02a4134f8b1d5d3d7b5b7
Dipkiran Shrestha <thesillyghoul@gmail.com>
2026-03-14 18:19:04 +0545

    refactor: simplify plugin main

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
-    main = "nvim-treesitter.config",
     opts = {
       ensure_installed = { "lua", "python", "javascript", "typescript" },
       auto_install = true,

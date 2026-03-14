e790c5d6bb5c199ee3ff8b5c881311f4c06df432
Dipkiran Shrestha <thesillyghoul@gmail.com>
2026-03-14 19:23:16 +0545

    feat: adopt rolling versions

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,10 +1,8 @@
 return {
   "neovim/nvim-lspconfig",
-  version = "v1.*",
   dependencies = {
     {
       "saghen/blink.cmp",
-      version = "v0.*",
       dependencies = { "rafamadriz/friendly-snippets" },
       opts = {
         fuzzy = { implementation = "lua" },
@@ -13,13 +11,10 @@ return {
     { "williamboman/mason.nvim", opts = {} },
     {
       "williamboman/mason-lspconfig.nvim",
-      version = "1.*",
       opts = {
         handlers = {
-          function(server)
-            require("lspconfig")[server].setup({
-              capabilities = require("blink.cmp").get_lsp_capabilities(),
-            })
+          function(server_name)
+            vim.lsp.enable(server_name)
           end,
         },
       },
